f2763ac8801667b88f840ab1835b361fd5bcdf09
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-04-02 01:49:31 +0300

    feat(dashboard): add employees stats cards with presence tracking and status indicators

--- components/dashboard/employees/EmployeesStats.tsx
+++ components/dashboard/employees/EmployeesStats.tsx
@@ -10,6 +10,7 @@ import {
 import {
   AlertTriangleIcon,
   BadgeCheckIcon,
+  LaptopIcon,
   PartyPopperIcon,
   UserCheck2Icon,
   UserIcon,
@@ -25,67 +26,78 @@ export default function EmployeesStats() {
   const employeesPresentPercentage = (employeesPresent / totalEmployees) * 100;
 
   return (
-    <div className="grid lg:grid-cols-3 gap-4">
-      <Card>
-        <CardHeader className="pb-2">
-          <CardTitle className="text-base">Total employees</CardTitle>
-        </CardHeader>
-        <CardContent className="flex justify-between items-center">
-          <div className="flex gap-2">
-            <UserIcon />
-            <div className="text-5xl font-bold">{totalEmployees}</div>
-          </div>
-          <div>
-            <Button size="xs" asChild>
-              <Link href="/dashboard/employees">View all</Link>
-            </Button>
-          </div>
-        </CardContent>
-      </Card>
-      <Card>
-        <CardHeader className="pb-2">
-          <CardTitle className="text-base">Employees present</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <div className="flex gap-2">
+    <>
+      <div className="grid lg:grid-cols-3 gap-4">
+        <Card>
+          <CardHeader className="pb-2">
+            <CardTitle className="text-base">Total employees</CardTitle>
+          </CardHeader>
+          <CardContent className="flex justify-between items-center">
+            <div className="flex gap-2">
+              <UserIcon />
+              <div className="text-5xl font-bold">{totalEmployees}</div>
+            </div>
+            <div>
+              <Button size="xs" asChild>
+                <Link href="/dashboard/employees">View all</Link>
+              </Button>
+            </div>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader className="pb-2">
+            <CardTitle className="text-base">Employees present</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div className="flex gap-2">
+              {employeesPresentPercentage > 75 ? (
+                <UserCheck2Icon />
+              ) : (
+                <UserRoundXIcon />
+              )}
+              <div className="text-5xl font-bold">{employeesPresent}</div>
+            </div>
+          </CardContent>
+          <CardFooter>
             {employeesPresentPercentage > 75 ? (
-              <UserCheck2Icon />
+              <span className="text-xs text-green-500 flex gap-1 items-center">
+                <BadgeCheckIcon />
+                {employeesPresentPercentage}% of employees are present
+              </span>
             ) : (
-              <UserRoundXIcon />
+              <span className="text-xs text-red-500 flex gap-1 items-center">
+                <AlertTriangleIcon />
+                Only {employeesPresentPercentage}% of employees are present
+              </span>
             )}
-            <div className="text-5xl font-bold">{employeesPresent}</div>
-          </div>
-        </CardContent>
-        <CardFooter>
-          {employeesPresentPercentage > 75 ? (
-            <span className="text-xs text-green-500 flex gap-1 items-center">
-              <BadgeCheckIcon />
-              {employeesPresentPercentage}% of employees are present
-            </span>
-          ) : (
-            <span className="text-xs text-red-500 flex gap-1 items-center">
-              <AlertTriangleIcon />
-              Only {employeesPresentPercentage}% of employees are present
-            </span>
-          )}
-        </CardFooter>
-      </Card>
-      <Card className="border-primary flex flex-col">
-        <CardHeader className="pb-2">
-          <CardTitle className="text-base">Employee of the month</CardTitle>
+          </CardFooter>
+        </Card>
+        <Card className="border-primary flex flex-col">
+          <CardHeader className="pb-2">
+            <CardTitle className="text-base">Employee of the month</CardTitle>
+          </CardHeader>
+          <CardContent className="flex gap-2 items-center">
+            <Avatar>
+              <Image src={cm} alt="Employee of the month avatar" />
+              <AvatarFallback>CM</AvatarFallback>
+            </Avatar>
+            <span className="text-2xl">Colin Murray!</span>
+          </CardContent>
+          <CardFooter className="flex gap-2 items-center text-xs text-muted-foreground mt-auto">
+            <PartyPopperIcon className="text-primary" />
+            <span>Congratulations, Colin!</span>
+          </CardFooter>
+        </Card>
+      </div>
+      <Card className="my-4">
+        <CardHeader>
+          <CardTitle className="text-lg flex items-center gap-2">
+            <LaptopIcon />
+            <span>Employee work location trends</span>
+          </CardTitle>
         </CardHeader>
-        <CardContent className="flex gap-2 items-center">
-          <Avatar>
-            <Image src={cm} alt="Employee of the month avatar" />
-            <AvatarFallback>CM</AvatarFallback>
-          </Avatar>
-          <span className="text-2xl">Colin Murray!</span>
-        </CardContent>
-        <CardFooter className="flex gap-2 items-center text-xs text-muted-foreground mt-auto">
-          <PartyPopperIcon className="text-primary" />
-          <span>Congratulations, Colin!</span>
-        </CardFooter>
+        <CardContent className="pl-0">Work Location Trends</CardContent>
       </Card>
-    </div>
+    </>
   );
 }
