80a1243e43daad6ef133138452a1bc2e8ca3d299
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-04-02 01:40:48 +0300

    feat: add employees presence stats with conditional status indicators

--- components/dashboard/employees/EmployeesStats.tsx
+++ components/dashboard/employees/EmployeesStats.tsx
@@ -7,13 +7,22 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { PartyPopperIcon, UserIcon } from "lucide-react";
+import {
+  AlertTriangleIcon,
+  BadgeCheckIcon,
+  PartyPopperIcon,
+  UserCheck2Icon,
+  UserIcon,
+  UserRoundXIcon,
+} from "lucide-react";
 import Link from "next/link";
 import cm from "@/public/images/cm.jpg";
 import Image from "next/image";
 
 export default function EmployeesStats() {
   const totalEmployees = 100;
+  const employeesPresent = 80;
+  const employeesPresentPercentage = (employeesPresent / totalEmployees) * 100;
 
   return (
     <div className="grid lg:grid-cols-3 gap-4">
@@ -33,7 +42,34 @@ export default function EmployeesStats() {
           </div>
         </CardContent>
       </Card>
-      <Card>Card 2</Card>
+      <Card>
+        <CardHeader className="pb-2">
+          <CardTitle className="text-base">Employees present</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <div className="flex gap-2">
+            {employeesPresentPercentage > 75 ? (
+              <UserCheck2Icon />
+            ) : (
+              <UserRoundXIcon />
+            )}
+            <div className="text-5xl font-bold">{employeesPresent}</div>
+          </div>
+        </CardContent>
+        <CardFooter>
+          {employeesPresentPercentage > 75 ? (
+            <span className="text-xs text-green-500 flex gap-1 items-center">
+              <BadgeCheckIcon />
+              {employeesPresentPercentage}% of employees are present
+            </span>
+          ) : (
+            <span className="text-xs text-red-500 flex gap-1 items-center">
+              <AlertTriangleIcon />
+              Only {employeesPresentPercentage}% of employees are present
+            </span>
+          )}
+        </CardFooter>
+      </Card>
       <Card className="border-primary flex flex-col">
         <CardHeader className="pb-2">
           <CardTitle className="text-base">Employee of the month</CardTitle>
