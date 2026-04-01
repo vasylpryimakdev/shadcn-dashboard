8e4003e7cbf15ebc8e29a90b8a8d7e5e59d41031
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-04-02 01:33:33 +0300

    feat: add employee stats cards and employee of the month section

--- components/dashboard/employees/EmployeesStats.tsx
+++ components/dashboard/employees/EmployeesStats.tsx
@@ -1,7 +1,16 @@
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { UserIcon } from "lucide-react";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { PartyPopperIcon, UserIcon } from "lucide-react";
 import Link from "next/link";
+import cm from "@/public/images/cm.jpg";
+import Image from "next/image";
 
 export default function EmployeesStats() {
   const totalEmployees = 100;
@@ -18,14 +27,29 @@ export default function EmployeesStats() {
             <div className="text-5xl font-bold">{totalEmployees}</div>
           </div>
           <div>
-            <Button size="lg" asChild>
+            <Button size="xs" asChild>
               <Link href="/dashboard/employees">View all</Link>
             </Button>
           </div>
         </CardContent>
       </Card>
       <Card>Card 2</Card>
-      <Card>Card 3</Card>
+      <Card className="border-primary flex flex-col">
+        <CardHeader className="pb-2">
+          <CardTitle className="text-base">Employee of the month</CardTitle>
+        </CardHeader>
+        <CardContent className="flex gap-2 items-center">
+          <Avatar>
+            <Image src={cm} alt="Employee of the month avatar" />
+            <AvatarFallback>CM</AvatarFallback>
+          </Avatar>
+          <span className="text-2xl">Colin Murray!</span>
+        </CardContent>
+        <CardFooter className="flex gap-2 items-center text-xs text-muted-foreground mt-auto">
+          <PartyPopperIcon className="text-primary" />
+          <span>Congratulations, Colin!</span>
+        </CardFooter>
+      </Card>
     </div>
   );
 }
