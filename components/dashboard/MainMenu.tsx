de56a9b168548c5412854d9d6de23dd83ee3a113
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-04-01 20:44:17 +0300

    feat: add dashboard sidebar nav links with active route highlighting

--- components/dashboard/MainMenu.tsx
+++ components/dashboard/MainMenu.tsx
@@ -1,5 +1,6 @@
 import { cn } from "@/lib/utils";
 import MenuTitle from "./MenuTitle";
+import MenuItem from "./MenuItem";
 
 export default function MainMenu({ className }: { className?: string }) {
   return (
@@ -9,6 +10,14 @@ export default function MainMenu({ className }: { className?: string }) {
       <header className="hidden md:block border-b dark:border-b-black border-b-zinc-300 pb-4">
         <MenuTitle />
       </header>
+
+      <ul className="py-4 grow">
+        <MenuItem href="/dashboard">My dashboard</MenuItem>
+        <MenuItem href="/dashboard/teams">Teams</MenuItem>
+        <MenuItem href="/dashboard/employees">Employees</MenuItem>
+        <MenuItem href="/dashboard/account">Account</MenuItem>
+        <MenuItem href="/dashboard/settings">Settings</MenuItem>
+      </ul>
     </nav>
   );
 }
