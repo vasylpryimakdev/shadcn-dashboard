0abff7c19f3a64dea194bcc836be4e0649ab7116
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-04-01 14:15:30 +0300

    refactor: move home page to logged-out route group and add guest auth CTAs

---
+++ app/(logged-out)/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const Homepage = () => {
+  return (
+    <div>
+      <h1>Support me</h1>
+      <Button>Log in</Button>
+      <Button>Sign up</Button>
+    </div>
+  );
+};
+
+export default Homepage;
