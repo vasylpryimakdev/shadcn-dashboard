0fed3549e4dd99151c6ecfa9c8dccf4b6c7ba2ed
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-04-02 02:15:50 +0300

    feat(dashboard): enhance teams stats card with metric layout and navigation

--- components/dashboard/teams/TeamStats.tsx
+++ components/dashboard/teams/TeamStats.tsx
@@ -1,11 +1,28 @@
+import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { ListChecksIcon } from "lucide-react";
+import { ListChecksIcon, UsersIcon } from "lucide-react";
+import Link from "next/link";
 
 export default function TeamsStats() {
   return (
     <>
       <div className="grid lg:grid-cols-3 gap-4">
-        <Card>Total teams</Card>
+        <Card>
+          <CardHeader className="pb-2">
+            <CardTitle className="text-base">Total teams</CardTitle>
+          </CardHeader>
+          <CardContent className="flex justify-between items-center">
+            <div className="flex gap-2">
+              <UsersIcon />
+              <div className="text-5xl font-bold">8</div>
+            </div>
+            <div>
+              <Button size="xs" asChild>
+                <Link href="/dashboard/teams">View all</Link>
+              </Button>
+            </div>
+          </CardContent>
+        </Card>
         <Card>
           <span>Team leaders</span>
         </Card>
