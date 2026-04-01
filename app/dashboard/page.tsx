8d0020bf31bead7c095fc542f313b4bff3dbd6b1
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-04-01 21:04:58 +0300

    feat: add tabs component and tabbed employees/teams stats on dashboard home

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,14 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
 export default function DashboardPage() {
-  return <div>Dashboard page</div>;
+  return (
+    <Tabs defaultValue="employees">
+      <TabsList className="mb-4">
+        <TabsTrigger value="employees">Employees stats</TabsTrigger>
+        <TabsTrigger value="teams">Teams stats</TabsTrigger>
+      </TabsList>
+      <TabsContent value="employees">Employees Stats</TabsContent>
+      <TabsContent value="teams">Team Stats</TabsContent>
+    </Tabs>
+  );
 }
